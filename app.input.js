5e8ffa47f5c597f2ce74fed45874b8ae441362a8
VexScythe <davide.ludovici@yahoo.it>
2024-03-29 22:54:59 +0100

    feat: app input handler

--- app.input.js
+++ app.input.js
@@ -3,3 +3,22 @@ import { render } from './siteUI.js';
 
 const pokemonInput = document.querySelector('[data-pokemon-input]')
 const searchBtn = document.querySelector('[data-search-btn]')
+
+export function init() {
+    searchBtn.addEventListener('click', () => {
+        searchPkmn()
+        pokemonInput.value = '';
+    });
+}
+
+async function searchPkmn() {
+    const pokemonName = pokemonInput.value.trim();
+    if(!pokemonName) return;
+    
+    try{
+        const pokemon = await getPokemon(pokemonName);
+        render(pokemon);
+    }catch(error) {
+        throw error;
+    }
+}
